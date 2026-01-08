7e3c2f7101c9c34e1bbda47f488a56445356e60f
Redarmy <148342133+yangchong99@users.noreply.github.com>
2026-01-09 02:34:39 +0800

    Update smzdm_cookie.js

--- Scripts/smzdm_cookie.js
+++ Scripts/smzdm_cookie.js
@@ -1,14 +1,15 @@
 // Quantumult X 持久化抓smzdm_cookie脚本
 // author: yangchong99
-// update-time: 2025-07-27 15:23:14
+// update-time: 2026-01-09
 
-if ($request && $request.url.match(/^https?:\/\/user-api\.smzdm\.com\/checkin$/)) {
+const url = $request.url;
+
+if (url.match(/^https?:\/\/user-api\.smzdm\.com\/checkin$/)) {
   const cookie = $request.headers.Cookie || $request.headers.cookie;
   if (cookie) {
     $prefs.setValueForKey(cookie, 'smzdm_cookie');
     $notify('什么值得买', '🍪 Cookie 获取成功', '已保存 Cookie');
   }
-  $done({});
 }
 
-
+$done({});
